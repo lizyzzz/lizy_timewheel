4ad10c15d6d8512da315c536fa8396a802ae9b42
Lizy <LizyZZZ@163.com>
2023-11-10 21:47:41 +0800

    finish func

--- include/timewheel.h
+++ include/timewheel.h
@@ -23,27 +23,22 @@ typedef std::function<void()> Task;
 // 定时任务类
 struct taskElement {
   Task task;  // 定时任务的函数
-  int pos;    // 定时任务挂载在环状数组中的索引位置
+  int pos;    // 定时任务挂clas载在环状数组中的索引位置
   int cycle;  // 定时任务的延迟轮次
   string key; // 定时任务的唯一标识 
 
-  bool operator==(const taskElement& etask) {
-    return key == etask.key;
-  }
-  bool operator!=(const taskElement& etask) {
-    return key != etask.key;
-  }
+  taskElement() = default;
+
   bool operator<(const taskElement& etask) {
     return key < etask.key;
-  }
-  bool operator>(const taskElement& etask) {
-    return key > etask.key;
   }
 };
 
 // 定时器类
 class TimeWheel {  
  public:
+  typedef std::shared_ptr<taskElement> pTask;
+
   /// @brief 时间轮构造函数
   /// @param slot 时间轮槽的数量 默认 slot 的数量是 10
   /// @param interval_time 时间轮运转的间隔(单位是 ms), 时间轮扫描间隔是 1s
@@ -79,52 +74,64 @@ class TimeWheel {
       (*task)();
     };
 
-    taskElement taskEle;
-    taskEle.task = func;
-    taskEle.key = key;
+    pTask taskEle = std::make_shared();
+    taskEle->task = func;
+    taskEle->key = key;
     // 根据时间确定该任务的位置
     std::pair<int, int> posAndCycle = getPosAndCycle(delayTime);
-    taskEle.pos = posAndCycle.first;
-    taskEle.cycle = posAndCycle.second;
+    taskEle->pos = posAndCycle.first;
+    taskEle->cycle = posAndCycle.second;
 
     // 添加到任务中
     {
       std::lock_guard<std::mutex> lk(task_lock);
       // 如果任务存在,先删除
       if (keyToTaskElement.count(key)) {
-        taskElement task_del = keyToTaskElement[key];
-        slots[task_del.pos].erase(task_del); 
+        pTask task_del = keyToTaskElement[key];
+        slots[task_del->pos].erase(task_del); 
       }
-      slots[taskEle.pos].insert(std::move(taskEle));
+      slots[taskEle->pos].insert(taskEle);
+      keyToTaskElement[key] = taskEle;
     }
 
     return res;
   }
 
-  // 删除定时任务
+  /// @brief 删除定时任务
+  /// @param key 定时任务的 key
   void RemoveTask(const string& key);
 
   // 以后台线程的方式启动定时器执行函数
   void Run();
 
+  // 关闭定时器
+  void Close();
+
  private:
+  TimeWheel(const TimeWheel& tw) = delete;
+  TimeWheel& operator=(const TimeWheel& tw) = delete;
+
   /// @brief 根据延迟时间获取位置和循环次数
   /// @param delay_time 延迟时间(ms)
   /// @return (pos, cycle)
   std::pair<int, int> getPosAndCycle(int delay_time);
 
-  std::thread back_thread; // 后台线程
-  std::mutex task_lock;    // 互斥量
+  // 后台线程执行的函数
+  void backGroundFunc();
+
+  std::thread back_thread;      // 后台线程
+  std::mutex task_lock;         // 互斥量
+  std::condition_variable cond; // 条件变量
 
   std::chrono::duration<int, std::milli> interval; // 时间轮运行时间间隔(单位时毫秒)
   int curSlot;  // 当前遍历到的环状数组的索引
 
   // 通过vector组成环状数组, 通过遍历数组的方式实现时间轮
   // 当定时任务较多时, 每个 slot 内可能有多个定时任务, 通过 set 组装
-  vector<set<taskElement>> slots; 
+  vector<set<pTask>> slots; 
 
-  // 定时任务 key 到任务节点的映射, 便于在 list 中删除节点
-  unordered_map<string, taskElement> keyToTaskElement;
+  // 定时任务 key 到任务节点的映射, 便于在 set 中删除节点
+  unordered_map<string, pTask> keyToTaskElement;
 
   bool closed;  // 停止的标记
 
--- src/timewheel.cc
+++ src/timewheel.cc
@@ -12,7 +12,19 @@ TimeWheel::TimeWheel(int slot, int interval_time) {
   // 初始化操作
   closed = true;
   interval = std::chrono::milliseconds(interval_time);
-  slots.resize(slot, set<taskElement>());
+  slots.resize(slot, set<pTask>());
+}
+
+TimeWheel::~TimeWheel() {
+  Close();
+  if (back_thread.joinable()) {
+    back_thread.join();
+  }
+}
+
+void TimeWheel::Close() {
+  closed = true;
+  cond.notify_one();
 }
 
 std::pair<int, int> TimeWheel::getPosAndCycle(int delay_time) {
@@ -23,4 +35,56 @@ std::pair<int, int> TimeWheel::getPosAndCycle(int delay_time) {
   result.second = delay_time / (slots.size() * interval.count());
 
   return result;
+}
+
+void TimeWheel::RemoveTask(const string& key) {
+  std::lock_guard<std::mutex> lk(task_lock);
+  if (keyToTaskElement.count(key)) {
+    pTask task_del = keyToTaskElement[key];
+    slots[task_del->pos].erase(task_del);
+    keyToTaskElement.erase(key);
+  }
+}
+
+void TimeWheel::Run() {
+  if (!closed) {
+    return;
+  }
+
+  curSlot = 0;
+  closed = false;
+  // 开启后台线程
+  std::thread t(&TimeWheel::backGroundFunc, this);
+  back_thread = std::move(t);
+  
+  return;
+}
+
+void TimeWheel::backGroundFunc() {
+  // 此处持有的锁会在 wait_until 处释放
+  std::unique_lock<std::mutex> lk(task_lock);
+  while (!closed) {
+    auto end = std::chrono::high_resolution_clock::now() + interval;
+    for (auto it = slots[curSlot].begin(); it != slots[curSlot].end(); ) {
+      if ((*it)->cycle > 0) {
+        (*it)->cycle--;
+        ++it;
+        continue;
+      }
+
+      // 执行任务
+      pTask taskNode = *it;
+      it = slots[curSlot].erase(it);
+      // 不要给耗时过长的定时任务, 否则会导致线程阻塞
+      taskNode->task();
+    }
+
+    // 推进时间轮
+    curSlot = (curSlot + 1) % slots.size();
+
+    // 等待并释放锁
+    cond.wait_until(lk, end, [this] {
+      return this->closed;
+    });
+  }
 }
